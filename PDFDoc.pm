37774ebe2ee7284febe0292e97b5067d2865d74a
Eugeniy Meshcheryakov <eugen@debian.org>
2019-07-03 18:50:02 +0200

    Improve documentation

--- PDFDoc.pm
+++ PDFDoc.pm
@@ -14,6 +14,14 @@ Encode::PDFDoc - PDFDocEncoding support
 
 =head1 SYNOPSIS
 
+       use Encode qw(encode decode)
+       use Encode::PDFDoc;
+
+       $characters = decode('PDFDoc', $octets);
+       $octets     = encode('PDFDoc', $characters);
+
+=head1 ABSTRACT
+
 This module contains support for PDFDocEncoding used for text strings
 in PDF documents outside of the document's content streams.
 This encoding is described in the PDF 1.7 specification, Annex D.
@@ -21,5 +29,12 @@ This encoding is described in the PDF 1.7 specification, Annex D.
 =head1 SEE ALSO
 
 L<Encode>
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright (C) 2019 Ievgenii Meshcheriakov <eugen@debian.org>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
