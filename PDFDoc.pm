b21534c7fa02cb7717796875c328b54a26011817
Eugeniy Meshcheryakov <eugen@debian.org>
2019-07-04 01:12:13 +0200

    Use Dist::Zilla

--- PDFDoc.pm
+++ PDFDoc.pm
@@ -1,4 +1,5 @@
 package Encode::PDFDoc;
+# ABSTRACT: PDFDocEncoding support
 our $VERSION = "0.01";
 
 use Encode;
@@ -8,9 +9,7 @@ XSLoader::load(__PACKAGE__,$VERSION);
 1;
 __END__
 
-=head1 NAME
-
-Encode::PDFDoc - PDFDocEncoding support
+=pod
 
 =head1 SYNOPSIS
 
@@ -20,7 +19,7 @@ Encode::PDFDoc - PDFDocEncoding support
        $characters = decode('PDFDoc', $octets);
        $octets     = encode('PDFDoc', $characters);
 
-=head1 ABSTRACT
+=head1 DESCRIPTION
 
 This module contains support for PDFDocEncoding used for text strings
 in PDF documents outside of the document's content streams.
@@ -29,12 +28,5 @@ This encoding is described in the PDF 1.7 specification, Annex D.
 =head1 SEE ALSO
 
 L<Encode>
-
-=head1 COPYRIGHT AND LICENCE
-
-Copyright (C) 2019 Ievgenii Meshcheriakov <eugen@debian.org>
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
 
 =cut
